58d68ab60f6317481e9da12500b96bfe08c4fb04
Fahad Murtaza <fahad@propertyfinder.ae>
2014-11-27 11:18:27 +0400

    Adding some basic codeception stuff
    Adding some codeception stuff, basically the packages.

---
+++ vendor/codeception/codeception/src/Codeception/Lib/Interfaces/Remote.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Codeception\Lib\Interfaces;
+
+interface Remote
+{
+    /**
+     * Sets 'url' configuration parameter to hosts subdomain.
+     * It does not open a page on subdomain. Use `amOnPage` for that
+     *
+     * ``` php
+     * <?php
+     * // If config is: 'http://mysite.com'
+     * // or config is: 'http://www.mysite.com'
+     * // or config is: 'http://company.mysite.com'
+     *
+     * $I->amOnSubdomain('user');
+     * $I->amOnPage('/');
+     * // moves to http://user.mysite.com/
+     * ?>
+     * ```
+     *
+     * @param $subdomain
+     *
+     * @return mixed
+     */
+    public function amOnSubdomain($subdomain);
+
+    /**
+     * Open web page at absolute URL.
+     * Base url will be reconfigured to use the host of provided Url.
+     *
+     * ``` php
+     * <?php
+     * $I->amOnUrl('http://codeception.com');
+     * $I->amOnPage('/quickstart'); // moves to http://codeception.com/quickstart
+     * ?>
+     * ```
+     */
+    public function amOnUrl($url);
+
+    public function _getUrl();
+}
